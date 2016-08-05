60374c01be7b0824e5c17563978f6a3949ba5ec3
Sean Rice <srice3@wisc.edu>
2016-08-05 15:05:56 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/sean/foodallergyscanner/EditAllergies.java
@@ -0,0 +1,13 @@
+package com.example.sean.foodallergyscanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditAllergies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_allergies);
+    }
+}
---
+++ app/src/main/java/com/example/sean/foodallergyscanner/Scanner.java
@@ -0,0 +1,13 @@
+package com.example.sean.foodallergyscanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Scanner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner);
+    }
+}
