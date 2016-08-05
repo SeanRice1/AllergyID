a53a7af2b2d87104fadecfd4543943d5c128b0b9
seanrice1 <bonehat5>
2016-08-05 17:50:14 -0400

    test

--- app/src/main/java/com/example/sean/foodallergyscanner/AllergyModel.java
+++ app/src/main/java/com/example/sean/foodallergyscanner/AllergyModel.java
@@ -2,4 +2,12 @@ package com.example.sean.foodallergyscanner;
 
 
 public class AllergyModel {
+
+    private String name;
+    private int value;
+
+    public AllergyModel(String name, int value){
+        this.name=name;
+        this.value=value;
+    }
 }
