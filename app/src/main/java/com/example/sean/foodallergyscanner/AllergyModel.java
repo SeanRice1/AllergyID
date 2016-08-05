90d66a04301b6a50cbb28a47626fbcbba623f41e
seanrice1 <bonehat5>
2016-08-05 18:07:44 -0400

    AllergyModel test

--- app/src/main/java/com/example/sean/foodallergyscanner/AllergyModel.java
+++ app/src/main/java/com/example/sean/foodallergyscanner/AllergyModel.java
@@ -4,10 +4,22 @@ package com.example.sean.foodallergyscanner;
 public class AllergyModel {
 
     private String name;
-    private int value;
+    // 0 = not checked, 1 = checked
+    private int checkedValue;
 
     public AllergyModel(String name, int value){
         this.name=name;
-        this.value=value;
+        checkedValue=value;
     }
+
+    public String getName(){
+        return name;
+    }
+    public void setChecked(){
+        if(checkedValue == 0)
+            checkedValue = 1;
+        else
+            checkedValue = 0;
+    }
+    
 }
