b3e827399394fb7409fde3cfe8b3548d8a5c59ca
seanrice1 <bonehat5>
2016-08-09 10:02:53 -0400

    crash before given permission, fine after

--- app/src/main/java/com/example/sean/foodallergyscanner/Scanner.java
+++ app/src/main/java/com/example/sean/foodallergyscanner/Scanner.java
@@ -1,18 +1,15 @@
 package com.example.sean.foodallergyscanner;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.graphics.Camera;
-import android.os.Build;
-import android.support.v4.util.SparseArrayCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v4.app.ActivityCompat;
 import android.util.SparseArray;
-import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.widget.Toast;
+
 
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
@@ -21,101 +18,92 @@ import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.IOException;
 
-public class Scanner extends AppCompatActivity {
+public class Scanner extends Activity {
 
-    private static final String TAG = "Barcode-reader";
+    SurfaceView surfaceView;
+    final static int CAMERA_REQUEST=5;
 
-//TODO: Figure out why camerasource is null
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
 
-        final SurfaceView surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
 
-        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this)
+        //int perm = ActivityCompat.checkSelfPermission(this,Manifest.permission.CAMERA);
+
+
+        createScanner();
+    }
+
+    private void createScanner() {
+        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this).build();
+
+                /*
+                .setBarcodeFormats(Barcode.UPC_E)
                 .setBarcodeFormats(Barcode.UPC_A)
-                .setBarcodeFormats(Barcode.UPC_E).build();
-
-        if (!barcodeDetector.isOperational()) {
-            // Note: The first time that an app using the barcode or face API is installed on a
-            // device, GMS will download a native libraries to the device in order to do detection.
-            // Usually this completes before the app is run for the first time.  But if that
-            // download has not yet completed, then the above call will not detect any barcodes
-            // and/or faces.
-            //
-            // isOperational() can be used to check if the required native libraries are currently
-            // available.  The detectors will automatically become operational once the library
-            // downloads complete on device.
-            Log.w(TAG, "Detector dependencies are not yet available.");
-
-            // Check for low storage.  If there is low storage, the native library will not be
-            // downloaded, so detection will not become operational.
-            IntentFilter lowstorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
-            boolean hasLowStorage = registerReceiver(null, lowstorageFilter) != null;
-
-            if (hasLowStorage) {
-                Toast.makeText(this, "Low Storage Error", Toast.LENGTH_LONG).show();
-            }
-        }
-
-        CameraSource.Builder builder = new CameraSource.Builder(getApplicationContext(),barcodeDetector)
-                .setFacing(CameraSource.CAMERA_FACING_BACK);
-/*
-        // make sure that auto focus is an available option
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            builder = builder.setFocusMode(
-                    autoFocus ? Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE : null);
-        }
+                .build();
 */
-        //System.out.println("@@@@@@: "+cameraSource.getPreviewSize());
+        final CameraSource cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedFps(15f)
+                .setRequestedPreviewSize(500, 500)
+                .build();
 
-        final CameraSource cameraSource = builder.build();
-
-
+                /*
+                .setFacing(CameraSource.CAMERA_FACING_BACK)
+                .setAutoFocusEnabled(true)
+                .build();
+*/
         surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
 
             @Override
-            public void surfaceCreated(SurfaceHolder holder) {
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
 
                 try {
-                    cameraSource.start(surfaceView.getHolder());
+                    if (ActivityCompat.checkSelfPermission(Scanner.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
 
-                } catch (IOException e) {
+                        ActivityCompat.requestPermissions(Scanner.this,new String[]{Manifest.permission.CAMERA},CAMERA_REQUEST);
+                        createScanner();
+                    }
+
+                    cameraSource.start(surfaceView.getHolder());
+                }
+                catch (IOException e){
                     e.printStackTrace();
                 }
-
-            }
-
-
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
             }
 
             @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
                 cameraSource.stop();
             }
         });
 
-
-
         barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
             @Override
             public void release() {
+
             }
 
             @Override
             public void receiveDetections(Detector.Detections<Barcode> detections) {
-                SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                SparseArray<Barcode> resultingBarcodes = detections.getDetectedItems();
 
-                Intent intent = new Intent(getApplicationContext(),Result.class);
-                intent.putExtra("UPC", barcodes.valueAt(0).displayValue);
+                if(resultingBarcodes.size()>0){
+                    Intent intent = new Intent(getApplicationContext(),Result.class);
+                    intent.putExtra("UPC",resultingBarcodes.valueAt(0).displayValue);
+                    startActivity(intent);
+                }
             }
-
         });
-
     }
+
 
 }
