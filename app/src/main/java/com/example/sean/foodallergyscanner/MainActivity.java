430433fbc7251121b2cd8e7c37097fca92a5ca25
seanrice1 <seanrice2015@gmail.com>
2016-08-20 17:58:44 -0500

    Color change on button press ( individual xml files)

--- app/src/main/java/com/example/sean/foodallergyscanner/MainActivity.java
+++ app/src/main/java/com/example/sean/foodallergyscanner/MainActivity.java
@@ -6,12 +6,15 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.RippleDrawable;
 import android.media.Image;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -114,4 +117,6 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
-}
+
+
+    }
