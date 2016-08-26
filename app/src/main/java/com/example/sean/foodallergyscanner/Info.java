4eaaa46581e98c39c023b75f32e436abc6617333
seanrice1 <seanrice2015@gmail.com>
2016-08-25 21:48:11 -0500

    info view outline

--- app/src/main/java/com/example/sean/foodallergyscanner/Info.java
+++ app/src/main/java/com/example/sean/foodallergyscanner/Info.java
@@ -1,7 +1,11 @@
 package com.example.sean.foodallergyscanner;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Info extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class Info extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        ImageView gitHubImage = (ImageView)findViewById(R.id.gitHubImage);
+
+        gitHubImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("www.github.com/seanrice1/FoodAllergyScanner"));
+                startActivity(intent);
+            }
+        });
+
     }
 }
