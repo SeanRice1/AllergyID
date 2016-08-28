d09539897b75f08919678ccee61ef0ee9fbf21aa
SeanRice1 <seanrice2015@gmail.com>
2016-08-28 15:17:30 -0500

    Fixed orientation, added links, formatted info text

--- app/src/main/java/com/example/sean/foodallergyscanner/Info.java
+++ app/src/main/java/com/example/sean/foodallergyscanner/Info.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Info extends AppCompatActivity {
 
@@ -15,15 +17,25 @@ public class Info extends AppCompatActivity {
         setContentView(R.layout.activity_info);
 
         ImageView gitHubImage = (ImageView)findViewById(R.id.gitHubLogo);
-        //TODO: format text
-        //TODO: this doesnt work
+        ImageView linkedInImage = (ImageView)findViewById(R.id.linkedInImage);
+        
+         TextView textView = (TextView) findViewById(R.id.info_text);
+         textView.setText(Html.fromHtml(getString(R.string.about_text)));
+
         gitHubImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                intent.setData(Uri.parse("www.github.com/seanrice1/FoodAllergyScanner"));
+                Intent intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.github.com/seanrice1/FoodAllergyScanner"));
+                startActivity(intent);
+            }
+        });
+
+        linkedInImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.linkedin.com/in/sean-rice"));
                 startActivity(intent);
             }
         });
